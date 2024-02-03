8f129b798df5a15a6100325be09213d1be30fd8a
Nick <n.assendelft@gmail.com>
2024-02-03 13:27:39 +0100

    Fixes project publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,18 +5,4 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.compose) apply false
     alias(libs.plugins.nexusPublish)
-    `maven-publish`
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            setUrl("https://maven.pkg.github.com/nassendelft/compose-win9x-theme")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
 }
--- win9x-theme/build.gradle.kts
+++ win9x-theme/build.gradle.kts
@@ -66,3 +66,16 @@ android {
 
     sourceSets["main"].resources.srcDirs("src/commonMain/resources")
 }
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            setUrl("https://maven.pkg.github.com/nassendelft/compose-win9x-theme")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
